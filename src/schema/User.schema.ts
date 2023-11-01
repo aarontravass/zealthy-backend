04f1ca138844e727f38be281c86db620dd77eeae
aarontravass <43901677+aarontravass@users.noreply.github.com>
2023-11-01 15:30:42 -0400

    Update User.schema.ts

--- src/schema/User.schema.ts
+++ src/schema/User.schema.ts
@@ -4,7 +4,6 @@ export const User = builder.prismaObject('User', {
   fields: (t) => ({
     id: t.exposeID('id'),
     email: t.exposeString('email'),
-    password: t.exposeString('password'),
     firstName: t.exposeString('firstName'),
     lastName: t.exposeString('lastName'),
     createdAt: t.expose('createdAt', { type: 'Date' }),
