e9f2c6a1c88c548aa06b43ff5550ff3670d479a0
aarontravass <43901677+aarontravass@users.noreply.github.com>
2023-11-01 03:10:58 +0000

    fix: added uuser to login schema

--- src/schema/LoginSession.schema.ts
+++ src/schema/LoginSession.schema.ts
@@ -1,10 +1,12 @@
 import { builder } from '../builder'
+import { User } from './User.schema'
 
 export const LoginSession = builder.prismaObject('LoginSession', {
   fields: (t) => ({
     authToken: t.exposeString('authToken'),
     refreshToken: t.exposeString('refreshToken'),
     createdAt: t.expose('createdAt', { type: 'Date' }),
-    updatedAt: t.expose('updatedAt', { type: 'Date' })
+    updatedAt: t.expose('updatedAt', { type: 'Date' }),
+    user: t.relation('user', { type: User })
   })
 })
